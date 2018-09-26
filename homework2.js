b5577a7662c356e49be698791e9d78c75e25afa9
annaharutyunyan2 <43216494+annaharutyunyan2@users.noreply.github.com>
2018-09-26 22:53:59 +0400

    Add files via upload

---
+++ homework2.js
@@ -0,0 +1,15 @@
+  const a = 1;
+  const b = 2;
+  const c = 3;
+  const d = "5";
+  const e = 10;
+
+  if (b+c===d) {
+  console.log ("Yay, you know basic math!")
+} 
+  else if (b*d===e) {
+  console.log ("Yay, you know how to multiply.")
+}
+  else {
+  console.log ("Sorry, math isn't your thing :/")
+  }
